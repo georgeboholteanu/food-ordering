f3fb1c3ba12965a65143f179c9bfd0ced2d85075
George Boholteanu <george.boholteanu@gmail.com>
2024-05-02 21:19:53 +0100

    gallery page

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,12 @@
+import GalleryContainer from "@/components/gallery/GalleryContainer";
+import React from "react";
+
+const Gallery = () => {
+	return (
+		<>
+			<GalleryContainer />
+		</>
+	);
+};
+
+export default Gallery;
---
+++ src/components/gallery/GalleryContainer.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./styles.module.css";
+import Link from "next/link";
+
+const GalleryContainer = () => {
+	return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Gallery</h1>
+            <div className={styles.galleryContainer}>                
+                <Link  className={`${styles.imageContainer} ${styles.gridSpan2}`} href="/gallery/img07.png"><img src="/gallery/img07.png" alt="gallery image" /></Link>
+                <Link  className={`${styles.imageContainer} ${styles.gridSpan1}`} href="/gallery/img02.png"><img src="/gallery/img02.png" alt="gallery image" /></Link>
+                <Link  className={`${styles.imageContainer} ${styles.gridSpan1}`} href="/gallery/img03.png"><img src="/gallery/img03.png" alt="gallery image" /></Link>
+                <Link  className={`${styles.imageContainer} ${styles.gridSpan1}`} href="/gallery/img04.png"><img src="/gallery/img04.png" alt="gallery image" /></Link>
+                <Link  className={`${styles.imageContainer} ${styles.gridSpan3}`} href="/gallery/img05.png"><img src="/gallery/img05.png" alt="gallery image" /></Link>
+                <Link  className={`${styles.imageContainer} ${styles.gridSpan1}`} href="/gallery/img06.png"><img src="/gallery/img06.png" alt="gallery image" /></Link>
+                <Link  className={`${styles.imageContainer} ${styles.gridSpan2}`} href="/gallery/img08.png"><img src="/gallery/img08.png" alt="gallery image" /></Link>
+                <Link  className={`${styles.imageContainer} ${styles.gridSpan1}`} href="/gallery/img09.png"><img src="/gallery/img09.png" alt="gallery image" /></Link>
+                <Link  className={`${styles.imageContainer} ${styles.gridSpan1}`} href="/gallery/img10.png"><img src="/gallery/img10.png" alt="gallery image" /></Link>
+                <Link  className={`${styles.imageContainer} ${styles.gridSpan1}`} href="/gallery/img01.png"><img src="/gallery/img01.png" alt="gallery image" /></Link>
+                <Link  className={`${styles.imageContainer} ${styles.gridSpan1}`} href="/gallery/img11.png"><img src="/gallery/img11.png" alt="gallery image" /></Link>
+                <Link  className={`${styles.imageContainer} ${styles.gridSpan1}`} href="/gallery/img12.png"><img src="/gallery/img12.png" alt="gallery image" /></Link>
+            </div>
+        </div>
+	);
+};
+
+export default GalleryContainer;
